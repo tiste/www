8514330982da44e8b9f197e699f8f69ad3f5f387
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2013-07-17 14:08:00 +0200

    Implement base

---
+++ source/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require vendor/jquery-2.0.3.min
+//= require vendor/bootstrap.min
+//= require vendor/realistic-typewriter
+//= require vendor/ss-social
+//= require javascript
---
+++ source/javascripts/javascript.js
@@ -0,0 +1,62 @@
+(function ($) {
+  $.fn.parallax = function (options) {
+    var defaults = {
+      "coef": 0.7
+    };
+
+    var opt = $.extend(defaults, options);
+
+    return this.each(function () {
+      var $$ = $(this);
+
+      $(window).bind("scroll", function () {
+        var offset = $$.offset();
+        var stop = offset.top + $$.height();
+        var windowTop = $(this).scrollTop();
+
+        if ((windowTop >= 0) && ((windowTop + $(this).height()) >= offset.top) && (windowTop <= stop)) {
+          newCoord = windowTop * opt.coef;
+
+          $$.css({
+            "background-position": "center -" + newCoord + "px"
+          });
+        }
+      });
+    });
+  }
+})(jQuery);
+
+function prompt(element, text) {
+  var span = document.createElement("span");
+  span.innerHTML = text;
+  element.appendChild(span);
+}
+
+function type_writer() {
+  var realisticTypewriter = new RealisticTypewriter(),
+      typeWriterElement   = document.getElementById("typewriter");
+
+  prompt(typeWriterElement, "Hi !");
+
+  setTimeout(function () {
+    prompt(typeWriterElement, "<br/>");
+    realisticTypewriter.type("I'm Baptiste Lecocq.", typeWriterElement, function () {
+      setTimeout(function () {
+        prompt(typeWriterElement, "<br/>");
+        realisticTypewriter.type("A 20yo french student developer.", typeWriterElement, function () {
+          prompt(typeWriterElement, "<br/>");
+          setTimeout(function () {
+            prompt(typeWriterElement, "<br/>");
+            realisticTypewriter.type("Oh, I love web development.", typeWriterElement);
+          }, 1000);
+        });
+      }, 1000);
+    });
+  }, 1000);
+}
+
+$(window).load(function () {
+  type_writer();
+
+  $(".me").parallax({coef: 0.7});
+});
