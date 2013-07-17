05f4e0416a304eb55656653c1e62970d22d9a834
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2013-07-17 18:42:41 +0200

    Add skill section

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require vendor/jquery-2.0.3.min
 //= require vendor/bootstrap.min
+//= require vendor/jquery.knob
 //= require vendor/realistic-typewriter
 //= require vendor/ss-social
 //= require javascript
--- source/javascripts/javascript.js
+++ source/javascripts/javascript.js
@@ -1,7 +1,7 @@
 (function ($) {
   $.fn.parallax = function (options) {
     var defaults = {
-      "coef": 0.7
+      'coef': 0.7
     };
 
     var opt = $.extend(defaults, options);
@@ -9,7 +9,7 @@
     return this.each(function () {
       var $$ = $(this);
 
-      $(window).bind("scroll", function () {
+      $(window).bind('scroll', function () {
         var offset = $$.offset();
         var stop = offset.top + $$.height();
         var windowTop = $(this).scrollTop();
@@ -18,7 +18,7 @@
           newCoord = windowTop * opt.coef;
 
           $$.css({
-            "background-position": "center -" + newCoord + "px"
+            'background-position': 'center -' + newCoord + 'px'
           });
         }
       });
@@ -27,27 +27,27 @@
 })(jQuery);
 
 function prompt(element, text) {
-  var span = document.createElement("span");
+  var span = document.createElement('span');
   span.innerHTML = text;
   element.appendChild(span);
 }
 
 function type_writer() {
   var realisticTypewriter = new RealisticTypewriter(),
-      typeWriterElement   = document.getElementById("typewriter");
+      typeWriterElement   = document.getElementById('typewriter');
 
-  prompt(typeWriterElement, "Hi !");
+  prompt(typeWriterElement, 'Hi !');
 
   setTimeout(function () {
-    prompt(typeWriterElement, "<br/>");
+    prompt(typeWriterElement, '<br/>');
     realisticTypewriter.type("I'm Baptiste Lecocq.", typeWriterElement, function () {
       setTimeout(function () {
-        prompt(typeWriterElement, "<br/>");
-        realisticTypewriter.type("A 20yo french student developer.", typeWriterElement, function () {
-          prompt(typeWriterElement, "<br/>");
+        prompt(typeWriterElement, '<br/>');
+        realisticTypewriter.type('A 20yo french student developer.', typeWriterElement, function () {
+          prompt(typeWriterElement, '<br/>');
           setTimeout(function () {
-            prompt(typeWriterElement, "<br/>");
-            realisticTypewriter.type("Oh, I love web development.", typeWriterElement);
+            prompt(typeWriterElement, '<br/>');
+            realisticTypewriter.type('Oh, I love web development.', typeWriterElement);
           }, 1000);
         });
       }, 1000);
@@ -55,8 +55,37 @@ function type_writer() {
   }, 1000);
 }
 
+function set_percent(language, percent) {
+  i = 0;
+  clear = setInterval(function () {
+    i++;
+    $(language).val(i).trigger('change');
+
+    if (i >= percent) {
+      clearInterval(clear);
+    }
+  }, 25);
+}
+
+$(function() {
+  $('.skill')
+    .knob({
+      readOnly: true
+    })
+    .trigger(
+      'configure', { 'fgColor': '#1abc9c', 'bgColor': '#FFFFFF' }
+    );
+
+  $('.skill').each(function () {
+    skill       = $(this);
+    skill_value = $(this).data('value');
+
+    set_percent(skill, skill_value);
+  });
+});
+
 $(window).load(function () {
   type_writer();
 
-  $(".me").parallax({coef: 0.7});
+  $('.me').parallax({coef: 0.7});
 });
