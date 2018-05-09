6c11402225ed2932afc33d204fbb05b506bd302b
Baptiste Lecocq <hello@tiste.io>
2018-05-09 17:36:34 +0200

    feat: manage offline/online mode

--- assets/main.js
+++ assets/main.js
@@ -21,4 +21,17 @@
             console.log('Service Worker registration failed: ', err);
         });
     }
+
+    'online offline'.split(' ').forEach((e) => {
+        window.addEventListener(e, networkMode, false);
+    });
+
+    function networkMode() {
+        if (navigator.onLine) {
+            document.body.classList.remove('offline');
+        }
+        else {
+            document.body.classList.add('offline');
+        }
+    }
 })();
