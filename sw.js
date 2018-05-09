d5b5ed5012f6fead88ea7637288bab66b45a1c23
Baptiste Lecocq <hello@tiste.io>
2018-05-09 17:18:27 +0200

    feat: allow website to be cached offline

---
+++ sw.js
@@ -0,0 +1,51 @@
+const cacheName = 'v1';
+
+const assetsToCache = [
+    '/assets/main.css',
+    '/assets/waves.js',
+    '/assets/main.js',
+    '/index.html',
+    '/en.html',
+    '/projects.html',
+];
+
+self.addEventListener('install', (event) => {
+    console.log('SW - Installed', event);
+
+    event.waitUntil(
+        caches.open(cacheName)
+            .then((cache) => {
+                return cache.addAll(assetsToCache);
+            })
+            .then(() => {
+                return self.skipWaiting();
+            })
+    )
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('SW - Activated', event);
+
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') {
+        return;
+    }
+    if (/http:/.test(event.request.url)) {
+        return;
+    }
+
+    event.respondWith(
+        caches.open(cacheName).then((cache) => {
+            return fetch(event.request).then((networkResponse) => {
+                cache.put(event.request, networkResponse.clone());
+
+                return networkResponse;
+            }).catch(() => {
+                return cache.match(event.request);
+            })
+        })
+    )
+});
