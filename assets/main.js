8fd534da4de4c5b14f2ffec1c3df2683e22b7bc0
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2019-12-10 22:05:30 +0100

    refactor: update theme

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,4 @@
 (function () {
-    const c = document.getElementsByTagName('canvas')[0],
-        ctx = c.getContext('2d');
-
-    function initWaves() {
-        setup(10, ctx, window.innerWidth, document.querySelector('.hero').offsetHeight);
-    }
-
-    'resize click touchleave'.split(' ').forEach((e) => {
-        window.addEventListener(e, initWaves, false);
-    });
-
-
-    // offline mode
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js').then((serviceWorkerRegistration) => {
             console.log('Service Worker succesfully registered with scope: ', serviceWorkerRegistration.scope);
@@ -27,13 +14,10 @@
     function networkMode() {
         if (navigator.onLine) {
             document.body.classList.remove('offline');
-        }
-        else {
+        } else {
             document.body.classList.add('offline');
         }
     }
 
-
-    initWaves();
     networkMode();
 })();
