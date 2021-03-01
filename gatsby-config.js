58d577aa77cfc62a3db9112803e7dfab8bbd5007
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2021-03-01 18:20:25 +0100

    feat: add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,48 @@
 module.exports = {
   siteMetadata: {
-    title: `Baptiste Lecocq, ingénieur logiciel indépendant à Lille`,
+    title: "Baptiste Lecocq, ingénieur logiciel indépendant à Lille",
+    siteUrl: "https://tiste.io",
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "UA-42477577-1",
         anonymize: true,
       },
     },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `Baptiste Lecocq, ingénieur logiciel indépendant à Lille`,
-        short_name: `Baptiste Lecocq`,
-        start_url: `/`,
-        background_color: `#FFCE00`,
-        theme_color: `#FFCE00`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.svg`,
+        name: "Baptiste Lecocq, ingénieur logiciel indépendant à Lille",
+        short_name: "Baptiste Lecocq",
+        start_url: "/",
+        background_color: "#FFCE00",
+        theme_color: "#FFCE00",
+        display: "minimal-ui",
+        icon: "src/images/favicon.svg",
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `markdown`,
+        name: "markdown",
         path: `${__dirname}/content`,
       },
     },
-    `gatsby-transformer-remark`,
+    "gatsby-transformer-remark",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
