f185ccd5e02df66c89c8c63bbd52cf08855ba20c
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2021-03-30 11:15:42 +0200

    chore: enhance design

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../../images/logo.svg";
 
 export default () => (
-  <footer className="footer">
+  <footer className="footer section-gray">
     <div className="container content">
       <div className="columns is-centered">
         <div className="column is-4 has-text-centered">
