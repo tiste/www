d72ecc16b3517fedcec37e45c5a1075dcc4ff472
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2021-04-22 08:01:03 +0200

    refactor: update card

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,51 +4,43 @@ export function Card({ title, link, children, isApp }) {
   const [image, ...rest] = children;
   const footer = rest.pop();
 
+  let typeTags = {};
+  let categoryTags = {};
+  let linkTags = {};
+  let osTags = {};
+
+  if (isApp) {
+    typeTags = {
+      itemScope: true,
+      itemType: "http://schema.org/SoftwareApplication",
+    };
+    categoryTags = {
+      itemProp: "applicationCategory",
+      content: "MobileApplication",
+    };
+    linkTags = {
+      itemProp: "url",
+    };
+    osTags = {
+      itemProp: "operatingSystem",
+      content: "iOS, ANDROID",
+    };
+  }
+
   return (
-    <div
-      className="card"
-      {...(isApp
-        ? {
-            itemScope: true,
-            itemType: "http://schema.org/SoftwareApplication",
-          }
-        : {})}
-    >
+    <div className="card" {...typeTags}>
       <div className="card-image">
         <figure className="image">
           <a href={link}>{image}</a>
         </figure>
       </div>
-      <div
-        className="card-content"
-        {...(isApp
-          ? {
-              itemProp: "applicationCategory",
-              content: "MobileApplication",
-            }
-          : {})}
-      >
+      <div className="card-content" {...categoryTags}>
         <p className="title">
-          <a
-            href={link}
-            {...(isApp
-              ? {
-                  itemProp: "url",
-                }
-              : {})}
-          >
+          <a href={link} {...linkTags}>
             {title}
           </a>
         </p>
-        <div
-          className="content"
-          {...(isApp
-            ? {
-                itemProp: "operatingSystem",
-                content: "iOS, ANDROID",
-              }
-            : {})}
-        >
+        <div className="content" {...osTags}>
           {rest}
         </div>
         <div className="tags">
